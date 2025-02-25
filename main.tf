c8fe8863b9c35099be44f1a25f0d22fc926739a4
indhiraPriyadarsini <iipriyadarsini26@gmail.com>
2025-02-25 10:29:36 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "VPC Name"
+  type        = string
+}
